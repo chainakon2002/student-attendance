74103016e408dab757411b9b249faf1a865e734d
chainakon2002 <chainakon9@gmail.com>
2025-05-20 17:49:05 +0700

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,3 @@
-
-// firebase.js
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
@@ -7,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCZjgmW_gTERq0LWy38h2nBM7vf2wFwAYY",
   authDomain: "attendance-system-8f7b4.firebaseapp.com",
   projectId: "attendance-system-8f7b4",
-  storageBucket: "attendance-system-8f7b4.firebasestorage.app",
+  storageBucket: "attendance-system-8f7b4.appspot.com",
   messagingSenderId: "51509707659",
   appId: "1:51509707659:web:527bf843423375b108a58e",
   measurementId: "G-K7DR4CMH31"
